f6c7b650fcf4c3de47b5f9d5a67bd57751cb80e2
joseignacio <joseignacio@macbookair>
2018-10-08 02:00:29 +0200

    NEW: Stream builder to traverse a set of files. With tests

--- src/main/java/org/jid/pipelineutils/Main.java
+++
@@ -1,8 +0,0 @@
-package org.jid.pipelineutils;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hola mundo!!");
-    }
-}
---
+++ src/main/java/org/jid/pipelineutils/streams/StreamBuilderUtils.java
@@ -0,0 +1,53 @@
+package org.jid.pipelineutils.streams;
+
+import org.paumard.streams.StreamsUtils;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static pl.touk.throwing.ThrowingFunction.unchecked;
+
+public class StreamBuilderUtils {
+
+    /**
+     *
+     * Read the files line by line
+     *
+     * <pre>{@code
+     *      Path file0 = Files.of("a00", "a01", "a02", "a03");
+     *      Path file1 = Files.of("a10", "a11", "a12", "a13");
+     *      Path file2 = Files.of("a20", "a21", "a22", "a23");
+     *      Path file3 = Files.of("a30", "a31", "a32", "a33");
+     *      Stream<String> traversingStream = StreamBuilderUtils.newStreamTraverseFiles(",", file0, file1, file2, file3);
+     *      List<String> collect = traversingStream.map(st -> st.collect(Collectors.toList());
+     *      // The collect list is [["a00", "a10", "a20", "a30"],
+     *                            ["a01", "a11", "a21", "a31"],
+     *                            ["a02", "a12", "a22", "a32"],
+     *                            ["a03", "a13", "a23", "a33"]]
+     * }</pre>
+     *
+     * WARNINGS:
+     *  - Stream must be closed explicitly or inside a try-with-resources.
+     *  - It doesn't support paralellism since there are files read.
+     *  - If files.lenght == 1 then <code>IllegalArgumentException</code>. It must be at least 2 files.
+     *  - If one file has less rows it won't fail and it would create an empty string for that section
+     *
+     * @param separator: The lines of each file will be joined into a String using this separator.
+     * @param files: The files to iterate thorugh.
+     * @return Stream of Strings linked between them with the separator.
+     */
+    public static Stream<String> newStreamTraverseFiles(String separator, Path... files) {
+
+        Stream<String> fileStreams[] = Stream.of(files)
+                .map(unchecked(Files::lines))
+                .toArray(Stream[]::new);
+
+        return StreamsUtils.traverse(fileStreams)
+                .map(stream -> stream.collect(Collectors.joining(separator)))
+                .onClose(() -> Arrays.stream(fileStreams).forEach(Stream::close));
+    }
+
+}
