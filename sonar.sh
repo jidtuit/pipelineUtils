9af6dab0b9610e2bf0b4ceedd6fd3c1e0084e3b3
joseignacio <joseignacio@macbookAIR>
2018-10-16 21:35:28 +0200

    Remove sensitive data

--- sonar.sh
+++ sonar.sh
@@ -3,10 +3,11 @@
 echo "Starting sonarqube..."
 
 ./gradlew clean sonarqube \
-  -Dsonar.projectKey=jidtuit_pipelineUtils \
-  -Dsonar.organization=jidtuit-github \
-  -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=228663e3d74d5f9ccba77b895849b58b491208be
+  -Dsonar.projectKey=$SONAR_PROJECT_KEY \
+  -Dsonar.organization=$SONAR_ORGANIZATION \
+  -Dsonar.host.url=$SONAR_HOST_URL \
+  -Dsonar.login=$SONAR_TOKEN \
+  -Dsonar.branch.name=$TRAVIS_BRANCH
 
 
 echo "End of sonarqube task."
